81a6d5d3da0f1da8c43ce7b759d85b474668c361
gonzalodelgadoet32-star <gonzalo.delgadoet32@gmail.com>
2025-10-14 15:38:50 -0300

    Create Inner join 1.sql

---
+++ Inner join 1.sql
@@ -0,0 +1,24 @@
+SELECT
+    H.numero AS 'Numero_Habitacion',
+    H.tipo AS 'Tipo',
+    H.capacidad AS 'Capacidad_Max',
+    H.zona
+FROM
+    Habitaciones H
+WHERE
+    H.id_habitacion NOT IN (
+        -- Subconsulta: Encuentra los id_habitacion que *tienen* una reserva que CONFLICTÚA
+        SELECT
+            R.habitacion
+        FROM
+            Reservas R
+        WHERE
+            -- Condición de solapamiento de fechas:
+            -- La reserva R comienza antes de que termine mi rango DESEADO
+            R.fecha_entrada < '2025-11-05 12:00:00'
+            -- Y la reserva R termina después de que empiece mi rango DESEADO
+            AND R.fecha_salida > '2025-11-01 14:00:00'
+    )
+    AND H.estado = 'Disponible' -- Opcional: solo habitaciones con estado 'Disponible'
+ORDER BY
+    H.numero;
