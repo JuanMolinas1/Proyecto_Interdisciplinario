f9a8db97b0689904a6730b6e6261cb69d5a5018f
JuanMolinas1 <juan.molinaset32@gmail.com>
2025-10-14 16:12:36 -0300

    Delete Inner Join 2.sql

--- Inner Join 2.sql
+++
@@ -1,6 +0,0 @@
--- Clientes con más de 3 reservas en el último año.
-select Clientes.nombre, Clientes.apellido, count(Reservas.id_reserva) as cantidad_reservas
-from Clientes
-inner join Reservas on Clientes.id_cliente = Reservas.cliente
-group by Clientes.id_cliente
-having count(Reservas.id_reserva) > 3;
