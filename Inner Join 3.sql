17c013428d84130c1de96b0fd673efb1857ab66c
JuanMolinas1 <juan.molinaset32@gmail.com>
2025-10-14 14:33:20 -0300

    Update Inner Join 3.sql

--- Inner Join 3.sql
+++ Inner Join 3.sql
@@ -1,4 +1,9 @@
-select month(Reserva.fecha_entrada) as mes, sum(servicios.precio) as dinero_mes
+set lc_time_names = 'es_ES';
+
+select monthname(Reserva.fecha_entrada) as mes, sum(Servicios.precio) as dinero_mes
 from Reservas
-inner join Reservas_Servicios on Reserva.id_reserva = servicios_reserva.id_reserva
-inner join Servicios on Servicios.id_servicio = servicios_reserva
+inner join Reservas_Servicios on Reserva.id_reserva = Servicios_Reserva.id_reserva
+inner join Servicios on Servicios.id_servicio = Servicios_Reserva.id_servicio
+where year(Reservas.fecha_entrada) = 2024
+group by month(Reservas.fecha_entrada)
+order by month(Reservas.fecha_entrada);
