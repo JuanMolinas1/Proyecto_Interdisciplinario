0f7097a116ce146e340970e756576e402ef4a09f
gonzalodelgadoet32-star <gonzalo.delgadoet32@gmail.com>
2025-10-21 15:18:49 -0300

    Update CONSULTAS EXTRA.sql

--- CONSULTAS EXTRA.sql
+++ CONSULTAS EXTRA.sql
@@ -4,3 +4,9 @@ SELECT E.id_empleado
 FROM Empleados E 
 INNER JOIN Turnos T ON E.id_empleado = T.empleado 
 WHERE E.sector = 'seguridad' AND T.horario_salida < '11:00:00'; 
+
+-- Empleados que trabajen en el sector de seguridad y entren antes de las 11
+
+select nombre, sector, sueldo
+from Empleados
+where sector = "seguridad" and sueldo > 2000
