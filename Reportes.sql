b391a4dc8038b4903ee6eebdfb119013a3113006
PabloCardozo224 <pblodcardo@gmail.com>
2025-10-31 15:45:50 -0300

    Update Reportes.sql

--- Reportes.sql
+++ Reportes.sql
@@ -6,3 +6,19 @@ group by Servicios.tipo
 order by Veces_Contratado desc
 limit 1;
 
+
+
+Pablo Cardozo
+select
+	year(fecha_entrada) AS año,
+    case
+		WHEN MONTH(fecha_entrada) BETWEEN 1 AND 3 THEN 'Temporada 1( Enero-Marzo)'
+		WHEN MONTH(fecha_entrada) BETWEEN 4 AND 6 THEN 'Temporada 1( Abril-Junio)'
+		WHEN MONTH(fecha_entrada) BETWEEN 7 AND 9 THEN 'Temporada 1( Julio-Septiembre)'
+		WHEN MONTH(fecha_entrada) BETWEEN 10 AND 12 THEN 'Temporada 4( Octubre-Diciembre)'
+	END AS Temporada,
+	COUNT(*) as Cantidad_Reservas
+From Reservas
+group by Año, Temporada
+ORDER BY Año, temporada;
+     
