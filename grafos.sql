5ca01742aae554a185d75f26a12f4924a562d2bf
JuanMolinas1 <juan.molinaset32@gmail.com>
2025-11-04 15:57:36 -0300

    Update grafos.sql

--- grafos.sql
+++ grafos.sql
@@ -1,18 +1,18 @@
-SELECT
-    C.id_cliente AS Id_Cliente,
-    C.nombre AS Nombre_Cliente,
-    R.id_reserva AS Id_Reservas,
-    H.id_habitacion AS Id_habitacion,
-    H.zona AS Zona,
-    S.tipo AS ID_Servicios 
-FROM
-    Clientes C
-left JOIN
-    Reservas R ON C.id_cliente = R.cliente
-left JOIN
-    Habitaciones H ON R.habitacion = H.id_habitacion
-LEFT JOIN
-    Servicios_Reservas SR ON R.id_reserva = SR.reserva 
-left JOIN
-    Servicios S ON SR.servicio = S.id_servicio
+select
+    Clientes.id_cliente as Id_Cliente,
+    Clientes.nombre as Nombre_Cliente,
+    Reservas.id_reserva as Id_Reservas,
+    Habitaciones.id_habitacion as Id_habitacion,
+    Habitaciones.zona as Zona,
+    Servicios.tipo as Id_Servicios 
+from
+    Clientes
+left join
+    Reservas on Clientes.id_cliente = Reservas.cliente
+left join
+    Habitaciones on Reservas.habitacion = Habitaciones.id_habitacion
+left join
+    Servicios_Reservas on Reservas.id_reserva = Servicios_Reservas.reserva 
+left join
+    Servicios on Servicios_Reservas.servicio = Servicios.id_servicio
 order by Id_Cliente asc;
