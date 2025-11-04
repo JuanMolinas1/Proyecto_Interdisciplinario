dd015f1ad7240ed930baa9d16c53564fab6b547a
JuanMolinas1 <juan.molinaset32@gmail.com>
2025-11-04 17:05:28 -0300

    Update Main2.py

--- Main2.py
+++ Main2.py
@@ -1,4 +1,4 @@
-# === LIBRERÍAS Y VARIABLES GLOBALES ===
+# Librerias
 import mysql.connector
 from mysql.connector import errorcode
 import datetime
@@ -7,9 +7,11 @@ import heapq
 import json
 
 # Variables Globales
+# Para la conexion SQL
 cursor = None
 cnx = None
 
+# Las tablas
 Tabla_Fecha = []
 Tabla_Tipo = []
 Tabla_Reservas = []
@@ -18,17 +20,19 @@ Tabla_Hash = []
 Grafos = []
 Monticulo = []
 heap = []
+Registro_Servicio_Demandado = []
+Ocupacion_Temporada = []
 
 
-# === CONEXIÓN SQL ===
+# Conectar a la Base de Datos
 def Conectar_SQL():
     global cnx, cursor
     try:
         cnx = mysql.connector.connect(
-            user='root',
-            password='',
-            host='localhost',
-            database='hotel'
+            user = 'root',
+            password = '',
+            host = 'localhost',
+            database = 'hotel'
         )
         cursor = cnx.cursor(dictionary=True)
         print("Conexión establecida")
@@ -41,12 +45,121 @@ def Conectar_SQL():
             print(err)
 
 
-# === FUNCIONES COMUNES ===
+# Crear las tablas con consultas SQL
 def InnerJoin(consulta):
     cursor.execute(consulta)
     return cursor.fetchall()
 
 
+# Crear las tablas usando las consultas de antes
+def Crear_Tablas():
+    global Tabla_Fecha, Tabla_Tipo, Tabla_Reservas, Tabla_sinHash, Tabla_Hash, Grafos, Monticulo, heap, Registro_Servicio_Demandado, Ocupacion_Temporada
+
+    # Tablas de búsqueda binaria
+    Tabla_Fecha = InnerJoin(
+        'select Reservas.fecha_entrada '
+        'from Reservas '
+        'inner join Habitaciones on Reservas.habitacion = Habitaciones.id_habitacion '
+        'order by Reservas.fecha_entrada asc;'
+    )
+    Tabla_Tipo = InnerJoin(
+        'select Habitaciones.tipo '
+        'from Reservas '
+        'inner join Habitaciones on Reservas.habitacion = Habitaciones.id_habitacion '
+        'order by Reservas.fecha_entrada asc;'
+    )
+    Tabla_Reservas = InnerJoin(
+        'select Habitaciones.numero, Habitaciones.zona, Habitaciones.tipo, Reservas.fecha_entrada '
+        'from Reservas '
+        'inner join Habitaciones on Reservas.habitacion = Habitaciones.id_habitacion '
+        'order by Reservas.fecha_entrada asc;'
+    )
+
+
+    # Tabla hash
+    Tabla_sinHash = InnerJoin(
+        'select Clientes.id_cliente as Identificador, Clientes.dni as DNI, Clientes.nombre as Nombre, '
+        'count(Reservas.id_reserva) as Cant_Reservas '
+        'from Reservas '
+        'inner join Clientes on Reservas.cliente = Clientes.id_cliente '
+        'group by Clientes.id_cliente, Clientes.dni, Clientes.nombre '
+        'having count(Reservas.id_reserva) >= 2;'
+    )
+
+    # Conversión a Hash
+    cant_registros = len(Tabla_sinHash)
+    Tabla_Hash = []
+    for fila in Tabla_sinHash:
+        fila_hash = {llave: hash(valor) % cant_registros for llave, valor in fila.items()}
+        Tabla_Hash.append(fila_hash)
+
+    Grafos = InnerJoin(
+        "select " \
+        "Clientes.id_cliente as Id_Cliente, " \
+        "Clientes.nombre as Nombre_Cliente, " 
+        "Reservas.id_reserva as Id_Reserva, " 
+        "Habitaciones.id_habitacion as Id_Habitacion, " \
+        "Habitaciones.zona as Zona, " \
+        "Servicios.tipo as Servicio " \
+        "from Clientes " \
+        "left join Reservas on Clientes.id_cliente = Reservas.cliente " \
+        "left join Habitaciones on Reservas.habitacion = Habitaciones.id_habitacion " \
+        "left join Servicios_Reservas on Reservas.id_reserva = Servicios_Reservas.reserva " \
+        "left join Servicios on Servicios_Reservas.servicio = Servicios.id_servicio " \
+        "order by Id_Cliente asc; " \
+    )
+
+
+    # Montículo
+    Monticulo = InnerJoin(
+        'select Clientes.id_cliente, Clientes.nombre, Clientes.vip, Habitaciones.tipo as habitacion, Reservas.precio '
+        'from Clientes '
+        'inner join Reservas on Clientes.id_cliente = Reservas.cliente '
+        'inner join Habitaciones on Reservas.habitacion = Habitaciones.id_habitacion;'
+    )
+
+    # Crear las Prioridades
+    heap = [
+        (
+            -int(c["vip"]),
+            -int(c["habitacion"].lower() == "presidencial"),
+            -int(c["habitacion"].lower() == "suite"),
+            -c["precio"],
+            c["id_cliente"],
+            c
+        )
+        for c in Monticulo
+    ]
+
+    heapq.heapify(heap)
+    print("Tablas creadas correctamente.")
+
+    Registro_Servicio_Demandado = InnerJoin(
+        "select Servicios.tipo as Tipo_Servicio, count(Servicios_Reservas.servicio) as Veces_Contratado "
+        "from Servicios_Reservas "
+        "join Servicios on Servicios_Reservas.servicio = Servicios.id_servicio "
+        "group by Servicios.tipo "
+        "order by Veces_Contratado desc "
+        "limit 1; "
+    )
+
+    Ocupacion_Temporada = InnerJoin(
+        "select "
+        "year(fecha_entrada) AS Temporada, "
+        "case "
+        "when month(fecha_entrada) between 1 and 3 then 'Parte 1 (Enero - Marzo)' "
+        "when month(fecha_entrada) between 4 and 6 then 'Parte 2 (Abril - Junio)' "
+        "when month(fecha_entrada) between 7 and 9 then 'Parte 3 (Julio - Septiembre)' "
+        "when month(fecha_entrada) between 10 and 12 then 'Parte 4 (Octubre - Diciembre)' "
+        "end AS Parte, "
+        "count(*) as Cantidad_Reservas "
+        "from Reservas "
+        "group by Temporada, Parte "
+        "order BY Temporada, Parte; "
+    )
+
+
+# Impresion de Tablas con formato
 def Imprimir_Tabla(tabla, titulo):
     if not tabla:
         print(f"{titulo} N/a")
@@ -68,9 +181,8 @@ def Imprimir_Tabla(tabla, titulo):
         print(Formatear_Fila(fila))
 
 
-# === CREAR TABLAS ===
-    # Grafos
-# === BÚSQUEDA BINARIA ===
+# Punto 2.1 Busqueda Binaria para buscar reservas segun fecha de inicio y tipo de habitación
+# Busqueda Binaria para fecha pedida
 def Busqueda_Binaria(tabla, fecha_busqueda):
     inicio, fin = 0, len(tabla) - 1
     while inicio <= fin:
@@ -84,7 +196,7 @@ def Busqueda_Binaria(tabla, fecha_busqueda):
             fin = medio - 1
     return False, None
 
-
+# Pedir fecha para buscar
 def Consultar_Fecha():
     while True:
         try:
@@ -96,7 +208,7 @@ def Consultar_Fecha():
         except ValueError:
             print("Por favor ingrese una fecha válida")
 
-
+# Pedir tipo para comparar
 def Consultar_Tipo():
     while True:
         tipo = input("Ingrese el tipo de habitación buscado (Standard/Deluxe/Presidencial): ").capitalize()
@@ -104,7 +216,7 @@ def Consultar_Tipo():
             return tipo
         print("Ingrese un tipo válido")
 
-
+# Comparacion de todos los valores finales
 def Busqueda():
     fecha = Consultar_Fecha()
     encontro, medio = Busqueda_Binaria(Tabla_Fecha, fecha)
@@ -120,18 +232,18 @@ def Busqueda():
         print("No se encontró una reserva con esa fecha.")
 
 
-# === TABLA HASH ===
+# Punto 2.2 Tabla Hash para Clientes frecuentes (>= 2 Reservas)
 def Mostrar_Hash():
     Imprimir_Tabla(Tabla_sinHash, "Tabla Sin Hash")
     Imprimir_Tabla(Tabla_Hash, "Tabla Hash")
 
 
-# === GRAFOS ===
+#Punto 2.3 Grafos para Clientes con Reservas y Servicios
 def Mostrar_Grafos():
     Imprimir_Tabla(Grafos, "Grafos")
 
 
-# === MONTÍCULO ===
+# Punto 2.4 Montículo para Reserva con mayor prioridad
 def Mostrar_Monticulo():
     if heap:
         _, _, _, _, _, cliente_top = heapq.heappop(heap)
@@ -143,7 +255,7 @@ def Mostrar_Monticulo():
         print("No hay clientes")
 
 
-# === INSERTAR RESERVA (PARTE 2) ===
+# Punto 3.1 Insertar Reserva nueva
 def Insertar_Reserva():
     print("\n-- Insertar Nueva Reserva --")
     habitacion = int(input("habitacion: "))
@@ -169,38 +281,14 @@ def Insertar_Reserva():
     cnx.commit()
     print("Reserva insertada correctamente.")
 
-    # Mostrar tabla actualizada
-    TablaActual = InnerJoin("select * from Reservas;")
-    Imprimir_Tabla(TablaActual, "Reservas Actualizadas")
 
-
-# === REPORTES JSON (PARTE 3) ===
+# Ejercicio 3.3 Reportes JSON
 def Reportes_JSON():
     # Servicio más demandado
-    Registro_Servicio_Demandado = InnerJoin(
-        "select Servicios.tipo as Tipo_Servicio, count(Servicios_Reservas.servicio) as Veces_Contratado "
-        "from Servicios_Reservas "
-        "join Servicios on Servicios_Reservas.servicio = Servicios.id_servicio "
-        "group by Servicios.tipo "
-        "order by Veces_Contratado desc "
-        "limit 1; "
-    )
+    
 
     # Ocupación por temporada
-    Ocupacion_Temporada = InnerJoin(
-        "select "
-        "year(fecha_entrada) AS Temporada, "
-        "case "
-        "when month(fecha_entrada) between 1 and 3 then 'Parte 1 (Enero - Marzo)' "
-        "when month(fecha_entrada) between 4 and 6 then 'Parte 2 (Abril - Junio)' "
-        "when month(fecha_entrada) between 7 and 9 then 'Parte 3 (Julio - Septiembre)' "
-        "when month(fecha_entrada) between 10 and 12 then 'Parte 4 (Octubre - Diciembre)' "
-        "end AS Parte, "
-        "count(*) as Cantidad_Reservas "
-        "from Reservas "
-        "group by Temporada, Parte "
-        "order BY Temporada, Parte; "
-    )
+    
 
     # Guardar archivos JSON
     with open("Servicio_Mas_Demandado.json", 'w', encoding='utf-8') as archivo:
@@ -211,11 +299,6 @@ def Reportes_JSON():
         json.dump(Ocupacion_Temporada, archivo, indent=4, ensure_ascii=False)
     print("El Reporte 'Ocupacion_Por_Temporada.json' fue creado con éxito")
 
-
-# === MAIN ===
-def main():
-    Conectar_SQL()
-    Crear_Tablas()
 
 # === MAIN ===
 def main():
